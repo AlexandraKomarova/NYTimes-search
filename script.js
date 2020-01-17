f4fbc3a62f4713afde7a47cfb4dbc4c6d4fcaff2
namcbc200 <namcbc200@gmail.com>
2020-01-17 12:13:48 -0500

    update with ajax request

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-alert()
+var apiKey = "zT3sAwoK5popRJdbPQ8S7olIC3zH8YDp";
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&begin_date=19800101&end_date=19900101&api-key=" + apiKey;
+
+$.ajax({
+    url:queryURL,
+    method: "GET"
+    }).then(function(response) {
+        console.log(response);
+    });
